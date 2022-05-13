6024e0f13bd8bb01f70afba6cab1d16a9d9ac0f1
hogehoge <elementrd6@hotmail.com>
2022-05-13 09:07:37 +0900

    carController 途中

---
+++ Assets/CarController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+}
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
